d79e8e6c041d297b1657205b1854b4af4a21297c
Alexander <al.x.falconer@gmail.com>
2020-12-08 17:30:13 -0500

    commit tree 9064ce8b236bbccb0b64c1b6406f7e3887d4e393

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+    []
 end
 
 def create_an_array
-  
+    ["Alex", "Cole", "Diane", "Scott"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  family = ["Alex", "Cole", "Diane", "Scott"]
+  family << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    family = ["Alex", "Cole", "Diane", "Scott", "arrays!"]
+    family.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    family = ["wow", "Alex", "Cole", "Diane", "Scott", "arrays!"]
+    array_family = family.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    family = ["wow", "Alex", "Cole", "Diane", "Scott", "arrays!"]
+    wow = family.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    family = ["wow", "Alex", "am", "Cole", "Diane", "Scott", "arrays!"]
+    family[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
-end
+    family = ["wow", "Alex", "am", "Cole", "Diane", "Scott", "arrays!"]
+    family.first
+end  
 
 def retrieve_last_element_from_array(array)
-  
+    family = ["wow", "Alex", "am", "Cole", "Diane", "Scott", "arrays!"]
+    family.last
 end
